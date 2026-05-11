b6341ad4ef425ce555127f4d9ec18a31004b61d6
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2026-05-10 20:00:38 -0700

    Fix configuration for Vite build output

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_portfolio',
-  build: {
-    outDir: 'dist',
-  }
+  base: '/my_portfolio'
 });
