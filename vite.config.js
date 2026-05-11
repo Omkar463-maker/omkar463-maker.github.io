6edbe0540890f33347042cc6b13f363125942eaf
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2026-05-10 20:02:25 -0700

    Change base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://omkar463-maker.github.io/my_portfolio/'
+  base: '/my_portfolio/'
 });
